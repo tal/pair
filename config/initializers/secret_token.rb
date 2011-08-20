119391018f92a9e79b973875f94e712ec3efdc84
Tal Atlas <tatlas@giltcity.com>
2011-08-20 01:35:56 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pair::Application.config.secret_token = '54c8e43e15a45fac2d7ab7271fb31ee72344b8e491be382ff9c263ab640ef12c0876a03a8e0d5d8ac4a3a46894253e3e2daa5e79ecd7705b49e4a619c61ffc1c'
