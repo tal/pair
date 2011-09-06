a626dca547a67b3241af56d841aecf05a04c3c27
Tal Atlas <me@tal.by>
2011-09-05 22:20:50 -0400

    fixed prod redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,7 @@
 DB = Sequel.connect(ENV["DATABASE_URL"])
-REDIS = Redis.new(:path => ENV["REDISTOGO_URL"])
+
+uri = URI.parse(ENV["REDISTOGO_URL"])
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
 Pair::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
