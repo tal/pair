ab4f776d4110176689d04190182a16c5ea9a4d09
Tal Atlas <me@tal.by>
2011-11-27 14:59:30 -0700

    nginx conf

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,12 @@
+require "bundler/capistrano"
+
 set :application, "pair"
 set :repository,  "git@github.com:Talby/pair.git"
 set :user, 'pair'
 
 set :scm, :git
 
-set :rvm_type, :user  # Copy the exact line. I really mean :user here
+set :rvm_type, :user
 
 role :web, "72.14.179.30"
 role :app, "72.14.179.30"
@@ -13,11 +15,10 @@ role :app, "72.14.179.30"
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    
+  end
+end
